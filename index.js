458c8e76b926bd24ef4d479abd0b2d2b15f49e22
jolly <vasanth_r@outlook.com>
2023-02-24 12:02:17 +0530

    Adds boilerplate code to try out Matter.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hi Maze');
+// Destructuring Matter
+const { Engine, Render, Runner, World, Bodies } = Matter;
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: { width: 800, height: 600 }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+// Adding a shape to the world
+const shape = Bodies.rectangle(200, 200, 50, 50, { isStatic: true });
+// Adding the shape to the world created
+World.add(world, shape);
