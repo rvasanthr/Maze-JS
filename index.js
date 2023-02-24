8f0d3d02d99b6babc797fe7efc390deec510cb7d
jolly <vasanth_r@outlook.com>
2023-02-24 13:39:57 +0530

    Adds multiple bodies of rectangular shape to the world. * Changes index.js.

--- index.js
+++ index.js
@@ -12,13 +12,18 @@ Runner.run(Runner.create(), engine);
 // Adding mouse constraint and mouse to world
 World.add(world, MouseConstraint.create(engine, { mouse: Mouse.create(render.canvas) }));
 // Adding a shapes to the world
+// The Wall
 const walls = [
     Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
     Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
     Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
     Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ];
-const shape = Bodies.rectangle(200, 200, 50, 50, { isStatic: false });
+// Other shapes
+// const shape = Bodies.rectangle(200, 200, 50, 50, { isStatic: false });
 // Adding objects to the world create
-World.add(world, shape);
+for (let i = 0; i < 20; i++) {
+    // World.add(world, shape);
+    World.add(world, Bodies.rectangle(200, 200, 50, 50, { isStatic: false }));
+}
 World.add(world, walls);
