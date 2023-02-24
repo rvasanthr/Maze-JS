cea78a2735ce11ece4479585342d2ce9d7d6513c
jolly <vasanth_r@outlook.com>
2023-02-24 14:03:55 +0530

    Adds polygon shapes to the world. Increases total cout of bodies. Update: index.js.

--- index.js
+++ index.js
@@ -25,13 +25,16 @@ const walls = [
 // Other shapes
 // const shape = Bodies.rectangle(200, 200, 50, 50, { isStatic: false });
 // Adding objects to the world create
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 45; i++) {
     // Randomizing object shapes with random
-    if (Math.random() > 0.51) {
+    const randomFloat = Math.random();
+    if (randomFloat >= 0.71) {
         // World.add(world, shape);
-        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, { isStatic: false }));
+        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 40, 40, { isStatic: false }));
+    } else if (randomFloat >= 0.51) {
+        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 30, { isStatic: false }));
     } else {
-        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, { isStatic: false }));
+        World.add(world, Bodies.polygon(Math.random() * width, Math.random() * height, 5, 35));
     }
 }
 World.add(world, walls);
